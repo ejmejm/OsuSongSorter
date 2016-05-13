86890304d72dfc778b0df567d4317222f5ccc2b9
Edan Meyer <ninjaedan@aol.com>
2016-05-13 14:24:54 -0500

    The entirety of V0.2 of the project. Allows users to decide to or not to keep each Osu! song in their library individually.

---
+++ Osu!_Song_Sorter/Osu! Song Sorter/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Osu__Song_Sorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.songPath = new System.Windows.Forms.TextBox();
+            this.newSongPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.skipButton = new System.Windows.Forms.Button();
+            this.songCountLabel = new System.Windows.Forms.Label();
+            this.editSongIndex = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.unfocus = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.artistLabel = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Below list the path of your Osu! songs folder";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // songPath
+            // 
+            this.songPath.Location = new System.Drawing.Point(15, 51);
+            this.songPath.Name = "songPath";
+            this.songPath.Size = new System.Drawing.Size(268, 20);
+            this.songPath.TabIndex = 1;
+            this.songPath.Text = "C:\\Program Files (x86)\\osu!\\Songs";
+            this.songPath.Visible = false;
+            // 
+            // newSongPath
+            // 
+            this.newSongPath.Location = new System.Drawing.Point(15, 111);
+            this.newSongPath.Name = "newSongPath";
+            this.newSongPath.Size = new System.Drawing.Size(268, 20);
+            this.newSongPath.TabIndex = 3;
+            this.newSongPath.Text = "C:\\Users\\Meyer Family\\Desktop\\OsuSongsTest";
+            this.newSongPath.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Below list the path for the mp3s";
+            this.label2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Begin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(113, 99);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 5;
+            this.playButton.Text = "Pause";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Visible = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(32, 128);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(111, 34);
+            this.addButton.TabIndex = 6;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Visible = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // skipButton
+            // 
+            this.skipButton.Location = new System.Drawing.Point(160, 129);
+            this.skipButton.Name = "skipButton";
+            this.skipButton.Size = new System.Drawing.Size(113, 34);
+            this.skipButton.TabIndex = 7;
+            this.skipButton.Text = "Skip";
+            this.skipButton.UseVisualStyleBackColor = true;
+            this.skipButton.Visible = false;
+            this.skipButton.Click += new System.EventHandler(this.skipButton_Click);
+            // 
+            // songCountLabel
+            // 
+            this.songCountLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.songCountLabel.AutoSize = true;
+            this.songCountLabel.Location = new System.Drawing.Point(95, 166);
+            this.songCountLabel.Name = "songCountLabel";
+            this.songCountLabel.Size = new System.Drawing.Size(93, 13);
+            this.songCountLabel.TabIndex = 8;
+            this.songCountLabel.Text = "Song x out of xxxx";
+            this.songCountLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.songCountLabel.Visible = false;
+            this.songCountLabel.Click += new System.EventHandler(this.songCountLabel_Click);
+            // 
+            // editSongIndex
+            // 
+            this.editSongIndex.AcceptsReturn = true;
+            this.editSongIndex.Location = new System.Drawing.Point(124, 162);
+            this.editSongIndex.Name = "editSongIndex";
+            this.editSongIndex.Size = new System.Drawing.Size(28, 20);
+            this.editSongIndex.TabIndex = 10;
+            this.editSongIndex.Text = "x";
+            this.editSongIndex.Visible = false;
+            this.editSongIndex.Enter += new System.EventHandler(this.editSongIndex_Enter);
+            this.editSongIndex.KeyDown += new System.Windows.Forms.KeyEventHandler(this.editSongIndex_KeyDown);
+            this.editSongIndex.Leave += new System.EventHandler(this.editSongIndex_Leave);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBar1.Location = new System.Drawing.Point(17, 48);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(268, 45);
+            this.trackBar1.TabIndex = 11;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // unfocus
+            // 
+            this.unfocus.AutoSize = true;
+            this.unfocus.Location = new System.Drawing.Point(-1, 7);
+            this.unfocus.Name = "unfocus";
+            this.unfocus.Size = new System.Drawing.Size(0, 13);
+            this.unfocus.TabIndex = 12;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(95, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(95, 13);
+            this.titleLabel.TabIndex = 13;
+            this.titleLabel.Text = "Now Playing - Title";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.titleLabel.Visible = false;
+            this.titleLabel.SizeChanged += new System.EventHandler(this.titleLabel_SizeChanged);
+            // 
+            // artistLabel
+            // 
+            this.artistLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.artistLabel.AutoSize = true;
+            this.artistLabel.Location = new System.Drawing.Point(121, 26);
+            this.artistLabel.Name = "artistLabel";
+            this.artistLabel.Size = new System.Drawing.Size(44, 13);
+            this.artistLabel.TabIndex = 14;
+            this.artistLabel.Text = "by Artist";
+            this.artistLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.artistLabel.Visible = false;
+            this.artistLabel.SizeChanged += new System.EventHandler(this.artistLabel_SizeChanged);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(32, 99);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(65, 45);
+            this.trackBar2.TabIndex = 15;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar2.Value = 50;
+            this.trackBar2.Visible = false;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 183);
+            this.Controls.Add(this.artistLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.unfocus);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.editSongIndex);
+            this.Controls.Add(this.songCountLabel);
+            this.Controls.Add(this.skipButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.newSongPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.songPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Name = "Form1";
+            this.Text = "Osu! Song Sorter Alpha V 0.2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox songPath;
+        private System.Windows.Forms.TextBox newSongPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button skipButton;
+        private System.Windows.Forms.Label songCountLabel;
+        private System.Windows.Forms.TextBox editSongIndex;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label unfocus;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label artistLabel;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ Osu!_Song_Sorter/Osu! Song Sorter/Form1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Osu__Song_Sorter
+{
+    public partial class Form1 : Form
+    {
+        int totalSongs = 0;
+        int songIndex = 1;
+        String currentMp3;
+        DirectoryInfo[] songFolders;
+        WMPLib.WindowsMediaPlayer wplayer;
+
+        public Form1()
+        {
+            wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.settings.setMode("loop", true);
+            InitializeComponent();
+            newSongPath.ReadOnly = false;
+            songPath.ReadOnly = false;
+            label1.Visible = true;
+            label2.Visible = true;
+            songPath.Visible = true;
+            newSongPath.Visible = true;
+            button1.Enabled = true;
+            button1.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            CustomTrackBar TrackBar1 = new CustomTrackBar(19,
+                new Size(300, 50));
+            this.Controls.Add(TrackBar1);
+            TrackBar1.BringToFront();
+             */
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(@songPath.Text))
+            {
+                label1.Text = "This path does not exist, please check it again.";
+                if (!Directory.Exists(@newSongPath.Text))
+                {
+                    label2.Text = "This path does not exist, please check it again.";
+                }
+            }
+            else if(!Directory.Exists(@newSongPath.Text))
+            {
+                label2.Text = "This path does not exist, please check it again.";
+            }
+            else
+            {
+                DirectoryInfo info = new DirectoryInfo(@songPath.Text);
+                DirectoryInfo[] tempFolders = info.GetDirectories();
+
+                //Check how many folders are a actually Osu! beatmaps
+                int fullCount = 0;
+                foreach(DirectoryInfo d in tempFolders)
+                {
+                    bool osuFile = false;
+                    foreach(FileInfo f in d.GetFiles())
+                    {
+                        if(f.Name.IndexOf(".osu") != -1)
+                        {
+                            osuFile = true;
+                        }
+                    }
+                    if(osuFile)
+                    {
+                        fullCount++;
+                    }
+                }
+
+                songFolders = new DirectoryInfo[fullCount];
+
+                for (int i = 0; i < fullCount; i++)
+                {
+                    songFolders[i] = tempFolders[i];
+                }
+
+                totalSongs = songFolders.Count();
+                newSongPath.ReadOnly = true;
+                songPath.ReadOnly = true;
+                label1.Visible = false;
+                label2.Visible = false;
+                songPath.Visible = false;
+                newSongPath.Visible = false;
+                button1.Enabled = false;
+                button1.Visible = false;
+                playButton.Visible = true;
+                addButton.Visible = true;
+                skipButton.Visible = true;
+                songCountLabel.Visible = true;
+                trackBar1.Visible = true;
+                artistLabel.Visible = true;
+                titleLabel.Visible = true;
+                trackBar2.Visible = true;
+
+                wplayer.settings.volume = trackBar2.Value;
+                playSong(songIndex);
+                timer1.Enabled = true;
+            }
+        }
+
+        private String findMp3Path(String folderPath)
+        {
+            FileInfo[] fileInfos = new DirectoryInfo(@folderPath).GetFiles();
+            String mp3Path = "";
+            long mp3Size = 0;
+            foreach(FileInfo f in fileInfos)
+            {
+                if(f.Name.IndexOf(".mp3") != -1 && f.Length > mp3Size)
+                {
+                    mp3Path = f.FullName;
+                    mp3Size = f.Length;
+                }
+            }
+            return mp3Path;
+        }
+        
+        private String findOsuPath(String folderPath)
+        {
+            FileInfo[] fileInfos = new DirectoryInfo(@folderPath).GetFiles();
+            foreach(FileInfo f in fileInfos)
+            {
+                if(f.Name.IndexOf(".osu") != -1)
+                {
+                    return f.FullName;
+                }
+            }
+            return null;
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            playPause();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            addSong();
+            unfocus.Focus();
+        }
+
+        private void skipButton_Click(object sender, EventArgs e)
+        {
+            skipSong();
+            unfocus.Focus();
+        }
+
+        private void songCountLabel_Click(object sender, EventArgs e)
+        {
+            editSongIndex.Visible = true;
+            editSongIndex.Focus();
+        }
+
+        private void editSongIndex_Leave(object sender, EventArgs e)
+        {
+            editSongIndex.Visible = false;
+            int n;
+            if (int.TryParse(editSongIndex.Text, out n) && songIndex <= totalSongs)
+            {
+                songIndex = n - 1;
+                skipSong();
+            }
+        }
+
+        private void editSongIndex_Enter(object sender, EventArgs e)
+        {
+            editSongIndex.Text = songIndex.ToString();
+            editSongIndex.BringToFront();
+        }
+
+        private void addSong()
+        {
+            timer1.Enabled = false;
+            trackBar1.Value = 0;
+            String newMp3Path = newSongPath.Text + "\\" + Path.GetFileName(@currentMp3);
+            File.Copy(@currentMp3, @newMp3Path, true);
+            TagLib.File newMp3 = TagLib.File.Create(@newMp3Path);
+            string osuInfo;
+            var fileStream = new FileStream(@findOsuPath(songFolders[songIndex - 1].FullName), FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                osuInfo = streamReader.ReadToEnd();
+            }
+            if (newMp3.Tag.Title == null)
+            {
+                newMp3.Tag.Title = osuInfo.Substring(osuInfo.IndexOf("Title:") + 6,
+                    osuInfo.IndexOf(System.Environment.NewLine, osuInfo.IndexOf("Title:") + 6) - osuInfo.IndexOf("Title:") - 6);
+            }
+            if (newMp3.Tag.AlbumArtists == null)
+            {
+                newMp3.Tag.AlbumArtists = new string[] { osuInfo.Substring(osuInfo.IndexOf("Artist:") + 7, 
+                    osuInfo.IndexOf(System.Environment.NewLine, osuInfo.IndexOf("Artist:") + 7) - osuInfo.IndexOf("Artist:") - 7) };
+            }
+            newMp3.Save();
+            songIndex++;
+            playSong(songIndex);
+        }
+
+        private void skipSong()
+        {
+            songIndex++;
+            playSong(songIndex);
+        }
+
+        private void editSongIndex_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                unfocus.Focus();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            unfocus.Focus();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if((int)((double) 1000 * wplayer.controls.currentPosition / wplayer.controls.currentItem.duration) >= 0
+                && (int)((double)1000 * wplayer.controls.currentPosition / wplayer.controls.currentItem.duration) <= 1000)
+            {
+                trackBar1.Value = (int)((double) 1000 * wplayer.controls.currentPosition / wplayer.controls.currentItem.duration);
+            }
+            else
+            {
+                trackBar1.Value = 0;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            wplayer.controls.currentPosition = wplayer.controls.currentItem.duration * 
+                (double) trackBar1.Value / (double) trackBar1.Maximum;
+        }
+
+        private void playPause()
+        {
+            if (playButton.Text == "Play")
+            {
+                wplayer.controls.play();
+                playButton.Text = "Pause";
+            }
+            else
+            {
+                wplayer.controls.pause();
+                playButton.Text = "Play";
+            }
+        }
+
+        private void playSong(int songNumber)
+        {
+            timer1.Enabled = false;
+            trackBar1.Value = 0;
+            if(songIndex < 1 || songIndex > totalSongs)
+            {
+                songIndex = 1;
+            }
+            currentMp3 = findMp3Path(songFolders[songIndex - 1].FullName);
+            string osuInfo;
+            String osuPath = findOsuPath(songFolders[songIndex - 1].FullName);
+            if (osuPath != null)
+            {
+                var fileStream = new FileStream(@osuPath, FileMode.Open, FileAccess.Read);
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    osuInfo = streamReader.ReadToEnd();
+                }
+            }
+            else
+            {
+                songIndex = 1;
+                var fileStream = new FileStream(@findOsuPath(songFolders[songIndex - 1].FullName), FileMode.Open, FileAccess.Read);
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    osuInfo = streamReader.ReadToEnd();
+                }
+            }
+            int previewTime = 0;
+            int.TryParse(osuInfo.Substring(osuInfo.IndexOf("PreviewTime:") + 12,
+                osuInfo.IndexOf(System.Environment.NewLine, osuInfo.IndexOf("PreviewTime:") + 12) - osuInfo.IndexOf("PreviewTime:") - 12), out previewTime);
+            wplayer.URL = currentMp3;
+            wplayer.controls.currentPosition = (double)previewTime / 1000.0;
+            playButton.Text = "Pause";
+            songCountLabel.Text = "Song " + songIndex + " out of " + totalSongs;
+            titleLabel.Text = getSongInfo("Title");
+            artistLabel.Text = "by " + getSongInfo("Artist");
+            timer1.Enabled = true;
+        }
+
+        private String getSongInfo(String key)
+        {
+            string osuInfo;
+            var fileStream = new FileStream(@findOsuPath(songFolders[songIndex - 1].FullName), FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                osuInfo = streamReader.ReadToEnd();
+            }
+            string info = osuInfo.Substring(osuInfo.IndexOf(key + ":") + (key.Length + 1),
+                osuInfo.IndexOf(System.Environment.NewLine, osuInfo.IndexOf(key + ":") + (key.Length + 1)) - osuInfo.IndexOf(key + ":") - (key.Length + 1));
+            return info;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Space && titleLabel.Visible)
+            {
+                playPause();
+                return true;
+            }
+            else if (keyData == Keys.Right)
+            {
+                songIndex++;
+                playSong(songIndex);
+                return true;
+            }
+            else if (keyData == Keys.Left)
+            {
+                songIndex--;
+                playSong(songIndex);
+                return true;
+            }
+            else if (keyData == Keys.Z)
+            {
+                addSong();
+                return true;
+            }
+            else if (keyData == Keys.X)
+            {
+                skipSong();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void titleLabel_SizeChanged(object sender, EventArgs e)
+        {
+            titleLabel.Left = (this.ClientSize.Width - titleLabel.Size.Width) / 2;
+        }
+
+        private void artistLabel_SizeChanged(object sender, EventArgs e)
+        {
+            artistLabel.Left = (this.ClientSize.Width - artistLabel.Size.Width) / 2;
+        }
+
+        private void trackBar1_MouseDown(object sender, MouseEventArgs e)
+        {
+            unfocus.Focus();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            wplayer.settings.volume = trackBar2.Value;
+        }
+    }
+}
